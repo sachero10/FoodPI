eb9a7a846f76072796cc908e85aa4b2fee9eb578
Alan Serén Almada <sachero10@gmail.com>
2023-07-26 21:45:36 -0300

    client

---
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>API FOOD</h1>
+      <button>Entrar</button>
+    </div>
+  );
+};
+
+export default LandingPage;
