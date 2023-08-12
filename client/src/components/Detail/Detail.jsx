393bbf407ab122d1e50907e90df0105d11e232d6
Alan Serén Almada <sachero10@gmail.com>
2023-08-11 22:44:45 -0300

    detail update

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -18,57 +18,58 @@ const Detail = () => {
 
   return (
     <div>
-      {Object.hasOwn(recipe, 'recipe') ?
-        (
-          <>
-            {/* {console.log("API")} */}
-            {recipe.recipe.title ? (
-              <>
-                <h1>Recipe: {recipe.recipe.title}</h1>
-                <img src={recipe.recipe.image} alt="recipe detail" />
-                <p>Summary: {recipe.recipe.summary}</p>
-                <h3>Health Score: {recipe.recipe.healthScore}</h3>
-                <div>
-                  <h4>Steps: </h4>
-                  {recipe.recipe.analyzedInstructions.steps.map((step, index) => (
+      {Object.hasOwn(recipe, "recipe") ? (
+        <>
+          {/* {console.log("API")} */}
+          {recipe.recipe.title ? (
+            <>
+              <h1>Recipe: {recipe.recipe.title}</h1>
+              <img src={recipe.recipe.image} alt="recipe detail" />
+              <p>Summary: {recipe.recipe.summary}</p>
+              <h3>Health Score: {recipe.recipe.healthScore}</h3>
+              <div>
+                <h4>Steps: </h4>
+                {recipe.recipe.analyzedInstructions[0].steps.map(
+                  (step, index) => (
                     <p key={index}>{step.step}</p>
-                  ))}
-                </div>
-                <div>
-                  <h4>Diets: </h4>
-                  {recipe.recipe.diets?.map((diet, index) => (
-                    <p key={index}>{diet}</p>
-                  ))}
-                </div>
-              </>
-            ) : (
-              <h3>Cargando...</h3>
-            )}
-          </>
-        ) : (
-          <>
-            {/* {console.log("BD")} */}
-            {recipe.recipe2.title ? (
-              <>
-                <h1>Recipe: {recipe.recipe2.title}</h1>
-                <img src={recipe.recipe2.image} alt="recipe detail" />
-                <p>Summary: {recipe.recipe2.summary}</p>
-                {/* <h3>Health Score: {recipe.recipe2.healthScore}</h3> */}
-                <div>
-                  <h4>Steps: {recipe.recipe2.steps}</h4>
-                </div>
-                {/* <div>
+                  )
+                )}
+              </div>
+              <div>
+                <h4>Diets: </h4>
+                {recipe.recipe.diets?.map((diet, index) => (
+                  <p key={index}>{diet}</p>
+                ))}
+              </div>
+            </>
+          ) : (
+            <h3>Cargando...</h3>
+          )}
+        </>
+      ) : (
+        <>
+          {console.log("BD")}
+          {/* {recipe.recipe2.title ? (
+            <>
+              <h1>Recipe: {recipe.recipe2.title}</h1>
+              <img src={recipe.recipe2.image} alt="recipe detail" />
+              <p>Summary: {recipe.recipe2.summary}</p> */}
+          {/* <h3>Health Score: {recipe.recipe2.healthScore}</h3> */}
+          {/* <div>
+                <h4>Steps: {recipe.recipe2.steps}</h4>
+              </div> */}
+          {/* <div>
                   <h4>Diets: </h4>
                   {recipe.diets?.map((diet, index) => (
                     <p key={index}>{diet}</p>
                   ))}
                 </div> */}
-              </>
-            ) : (
-              <h3>Cargando...</h3>
-            )}
-          </>)}
-
+          {/* </>
+          ) : (
+            <h3>Cargando...</h3>
+          )} */}
+        </>
+      )}
     </div>
   );
 };
--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,23 +1,30 @@
+import axios from "axios";
+
 export const GET_RECIPES = "GET_RECIPES";
 export const GET_DIETS = "GET_DIETS";
 export const GET_RECIPE_DETAIL = "GET_RECIPE_DETAIL";
 export const CLEAN_DETAIL = "CLEAN_DETAIL";
 
 export const getRecipes = () => {
-  return function (dispatch) {
-    fetch("http://localhost:3001/allrecipes")
-      .then((response) => response.json())
-      .then(({ recipes, recipesBDD }) =>
-        dispatch({ type: GET_RECIPES, payload: { recipes, recipesBDD } })
-      );
+  return async function (dispatch) {
+    const { data } = await axios("http://localhost:3001/allrecipes");
+    const { recipes, recipesBDD } = data;
+    return dispatch({ type: GET_RECIPES, payload: { recipes, recipesBDD } });
+    // fetch("http://localhost:3001/allrecipes")
+    //   .then((response) => response.json())
+    //   .then(({ recipes, recipesBDD }) =>
+    //     dispatch({ type: GET_RECIPES, payload: { recipes, recipesBDD } })
+    //   );
   };
 };
 
 export const getRecipeDetail = (id) => {
-  return function (dispatch) {
-    fetch(`http://localhost:3001/recipes/${id}`)
-      .then((response) => response.json())
-      .then((data) => dispatch({ type: GET_RECIPE_DETAIL, payload: data }));
+  return async function (dispatch) {
+    const { data } = await axios(`http://localhost:3001/recipes/${id}`);
+    return dispatch({ type: GET_RECIPE_DETAIL, payload: data });
+    //   fetch(`http://localhost:3001/recipes/${id}`)
+    //     .then((response) => response.json())
+    //     .then((data) => dispatch({ type: GET_RECIPE_DETAIL, payload: data }));
   };
 };
 
